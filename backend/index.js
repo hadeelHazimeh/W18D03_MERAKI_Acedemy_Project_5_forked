aed54b3a8f1ab0d06903450bb4250cb9d9d8333e
unknown <hadeel.f.hazimeh@gmail.com>
2024-02-11 15:54:57 +0300

    Data base pereprations A_10Hadeel

---
+++ backend/index.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+
+const app = express();
+const PORT = 5000;
+
+// Import Routers
+const roleRouter = require("./routers/roles");
+app.use(cors());
+app.use(express.json());
+
+// Routes Middleware
+app.use("/roles",roleRouter)
+
+// Handles any other endpoints [unassigned - endpoints]
+app.use("*", (req, res) => res.status(404).json("NO content at this path"));
+
+app.listen(PORT, () => {
+  console.log(`Server listening at http://localhost:${PORT}`);
+});
