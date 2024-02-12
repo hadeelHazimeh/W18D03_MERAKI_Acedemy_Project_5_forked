7645c85bd5c4f2eed3a57ac1d17d025ccf283416
AmalHawwari <Amal.Alhawwari@gmail.com>
2024-02-12 15:40:28 +0300

    amal

--- backend/models/schema.sql
+++ backend/models/schema.sql
@@ -1,83 +1,103 @@
 CREATE TABLE roles (
-    role_id SERIAL PRIMARY KEY,
-    role VARCHAR
+    role_id SERIAL NOT NULL,
+    role VARCHAR (255),
+    PRIMARY KEY (role_id)
 );
 
 CREATE TABLE permissions (
-    permissions_id SERIAL PRIMARY KEY,
-    permission VARCHAR
+    permissions_id SERIAL NOT NULL,
+    permission VARCHAR (255),
+    PRIMARY KEY (permissions_id)
 );
 
 CREATE TABLE role_permissions (
-    role_permissions_id SERIAL PRIMARY KEY,
-    role_id INTEGER,
-    permissions_id INTEGER,
+    role_permissions_id SERIAL NOT NULL,
+    role_id INT,
+    permissions_id INT,
     FOREIGN KEY (role_id) REFERENCES roles (role_id),
-    FOREIGN KEY (permissions_id) REFERENCES permissions (permissions_id)
+    FOREIGN KEY (permissions_id) REFERENCES permissions (permissions_id),
+    PRIMARY KEY (role_permissions_id)
 );
 
 CREATE TABLE users (
-    user_id SERIAL PRIMARY KEY,
-    userName VARCHAR,
-    email VARCHAR UNIQUE,
-    password VARCHAR(255),
-    is_deleted SMALLINT,
+    user_id SERIAL NOT NULL,
+    userName VARCHAR (255),
+    email VARCHAR(255) UNIQUE NOT NULL,
+    password VARCHAR (255),
+    is_deleted SMALLINT DEFAULT 0,
     role INT,
-    created_at TIMESTAMP
+    FOREIGN KEY (role) REFERENCES roles (role_id),
+    PRIMARY KEY (user_id)
 );
 
 CREATE TABLE events (
-    event_id SERIAL PRIMARY KEY,
-    event VARCHAR,
-    title VARCHAR,
-    image VARCHAR
+    event_id SERIAL NOT NULL,
+    event VARCHAR (255),
+    image VARCHAR (255),
+    is_deleted SMALLINT DEFAULT 0,
+    PRIMARY KEY (event_id)
 );
 
 CREATE TABLE services (
-    service_id SERIAL PRIMARY KEY,
-    service_name VARCHAR,
-    details VARCHAR,
+    service_id SERIAL NOT NULL,
+    service_name VARCHAR (255),
+    details VARCHAR (255),
     price INT,
-    image VARCHAR,
-    status VARCHAR,
-    provider INTEGER,
-    FOREIGN KEY (provider) REFERENCES users (user_id)
+    image VARCHAR (255),
+    status VARCHAR (255) DEFAULT 'pending',
+    provider INT,
+    is_deleted SMALLINT DEFAULT 0,
+    FOREIGN KEY (provider) REFERENCES users (user_id),
+    PRIMARY KEY (service_id)
 );
 
 CREATE TABLE service_event (
-    id SERIAL PRIMARY KEY,
-    service_id INTEGER,
-    event_id INTEGER,
+    id SERIAL NOT NULL,
+    service_id INT,
+    event_id INT,
     FOREIGN KEY (service_id) REFERENCES services (service_id),
-    FOREIGN KEY (event_id) REFERENCES events (event_id)
+    FOREIGN KEY (event_id) REFERENCES events (event_id),
+    is_deleted SMALLINT DEFAULT 0,
+    PRIMARY KEY (id)
 );
 
 CREATE TABLE package (
-    package_id SERIAL PRIMARY KEY,
-    package_Name VARCHAR,
-    price INTEGER,
-    Description VARCHAR,
-    image VARCHAR,
-    event INTEGER,
-    services INTEGER,
+    package_id SERIAL NOT NULL,
+    package_Name VARCHAR (255),
+    price INT,
+    Description VARCHAR (255),
+    image VARCHAR (255),
+    event INT,
+    services INT,
+    is_deleted SMALLINT DEFAULT 0,
     FOREIGN KEY (event) REFERENCES events (event_id),
-    FOREIGN KEY (services) REFERENCES services (service_id)
+    FOREIGN KEY (services) REFERENCES services (service_id),
+    PRIMARY KEY (package_id)
 );
 
 CREATE TABLE orders (
-    order_id SERIAL PRIMARY KEY,
-    order_price INTEGER,
-    user_id INTEGER,
-    created_at TIMESTAMP,
-    FOREIGN KEY (user_id) REFERENCES users (user_id)
+    order_id SERIAL NOT NULL,
+    order_price INT,
+    user_id INT,
+    eventDate VARCHAR(255),
+    place VARCHAR(255),
+    created_at TIMESTAMP DEFAULT NOW(),
+    is_deleted SMALLINT DEFAULT 0,
+    status VARCHAR(255) DEFAULT 'pending',
+    FOREIGN KEY (user_id) REFERENCES users (user_id),
+    PRIMARY KEY (order_id)
 );
 
 CREATE TABLE orders_services (
-    orders_services_id SERIAL PRIMARY KEY,
-    order_id INTEGER,
-    service_id INTEGER,
-    created_at TIMESTAMP,
+    orders_services_id SERIAL NOT NULL,
+    order_id INT,
+    service_id INT,
+    created_at TIMESTAMP DEFAULT NOW (),
+    is_deleted SMALLINT DEFAULT 0,
     FOREIGN KEY (order_id) REFERENCES orders (order_id),
-    FOREIGN KEY (service_id) REFERENCES services (service_id)
+
+    FOREIGN KEY (service_id) REFERENCES services (service_id),
+ 
+    PRIMARY KEY (orders_services_id)
 );
 
