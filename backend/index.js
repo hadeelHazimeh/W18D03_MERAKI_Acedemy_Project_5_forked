58f0b1ed80dbc9cbbced7b7e14aad3902c881691
unknown <hadeel.f.hazimeh@gmail.com>
2024-02-13 18:02:01 +0300

    createNewOrder

--- backend/index.js
+++ backend/index.js
@@ -6,12 +6,16 @@ const app = express();
 const PORT = 5000;
 
 // Import Routers
+
 const roleRouter = require("./routers/roles");
+const orderRouter=require("./routers/ordersRouter")
 app.use(cors());
 app.use(express.json());
 
 // Routes Middleware
 app.use("/roles",roleRouter)
+app.use("/orders",orderRouter)
+
 
 // Handles any other endpoints [unassigned - endpoints]
 app.use("*", (req, res) => res.status(404).json("NO content at this path"));
--- backend/routers/userRouter.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const userRouter = express.Router();
-const{register,login}=require("../controllers/users");
-userRouter.post("/register",register)
-userRouter.post("/login",login)
-
-
-module.exports = userRouter;
