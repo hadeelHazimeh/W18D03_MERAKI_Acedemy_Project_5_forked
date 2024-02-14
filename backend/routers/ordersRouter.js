e894535f8e650ab2b86519a0e85f8f818592709d
unknown <hadeel.f.hazimeh@gmail.com>
2024-02-14 18:39:00 +0300

    get All orders

--- backend/routers/ordersRouter.js
+++ backend/routers/ordersRouter.js
@@ -1,10 +1,10 @@
 const express = require("express");
-const { createNewOrder,createNewOrderServices } = require("../controllers/orderController");
+const { createNewOrder,createNewOrderServices,getAllOrders} = require("../controllers/orderController");
 const authentication = require("../middlewares/authentication");
 // Create roles router
 const orderRouter = express.Router();
 
 orderRouter.post("/create",authentication,createNewOrder);//client 
 orderRouter.post("/orderService",authentication,createNewOrderServices)//client 
-
+orderRouter.get("/allOrders",getAllOrders)//admin 
 module.exports = orderRouter;
--- backend/routers/service.js
+++ backend/routers/service.js
@@ -4,4 +4,5 @@ const authentication = require("../middlewares/authentication");
 const serviceRouter = express.Router();
 
 serviceRouter.post("/", authentication, createService);
+
 module.exports = serviceRouter;
