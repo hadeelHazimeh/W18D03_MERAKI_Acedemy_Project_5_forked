82d7cb59c3264254d36b785ba111a754a5bd7a57
unknown <hadeel.f.hazimeh@gmail.com>
2024-02-14 12:05:56 +0300

    createNewOrderServices

--- backend/index.js
+++ backend/index.js
@@ -18,6 +18,7 @@ app.use(express.json());
 app.use("/roles",roleRouter);
 app.use("/users",userRouter)
 app.use("/orders",orderRouter)
+app.use("/service",serviceRouter)
 // Handles any other endpoints [unassigned - endpoints]
 app.use("*", (req, res) => res.status(404).json("NO content at this path"));
 
