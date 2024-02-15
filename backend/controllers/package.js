77e70d9fa92b0de693f87e7f6ea2d509e549dbbf
AyaAlomarii <ayah.aliomari@gmail.com>
2024-02-16 00:54:49 +0300

    createNewServicePackage function

--- backend/controllers/package.js
+++ backend/controllers/package.js
@@ -1,18 +1,42 @@
-
 const { pool } = require("../models/db");
 // This function creates new package
 
 const createNewPackage = (req, res) => {
-    const { package_Name,price,Description,image,event } = req.body;
-    const value = [ package_Name,price,Description,image,event];
-    const query = `INSERT INTO package (package_Name,price,Description,image,event) VALUES ($1,$2,$3,$4,$5) RETURNING *;`;
+  const { package_Name, price, Description, image, event } = req.body;
+  const value = [package_Name, price, Description, image, event];
+  const query = `INSERT INTO package (package_Name,price,Description,image,event) VALUES ($1,$2,$3,$4,$5) RETURNING *;`;
+  pool
+    .query(query, value)
+    .then((result) => {
+      console.log(result.rows);
+      res.status(200).json({
+        success: true,
+        message: "package created successfully",
+        result: result.rows[0],
+      });
+    })
+    .catch((err) => {
+      res.status(500).json({
+        success: false,
+        message: "Server error",
+        err: err,
+      });
+    });
+};
+
+//======================================================================
+//this function for get all package with service details
+const createNewServicePackage = (req, res) => {
+    const { service_id,package_id } = req.body;
+    const value = [ service_id,package_id];
+    const query = `INSERT INTO service_package ( service_id,package_id) VALUES ($1,$2) RETURNING *;`;
     pool
       .query(query, value)
       .then((result) => {
         console.log(result.rows);
         res.status(200).json({
           success: true,
-          message: "package created successfully",
+          message: "service_package created successfully",
           result: result.rows[0],
         });
       })
@@ -24,11 +48,7 @@ const createNewPackage = (req, res) => {
         });
       });
   };
-  
-  //======================================================================
-  
-
-
 module.exports = {
-    createNewPackage,
+  createNewPackage,
+  createNewServicePackage
 };
--- backend/routers/packageRouter.js
+++ backend/routers/packageRouter.js
@@ -3,12 +3,15 @@ const express = require("express");
 // Create package router
 const packageRouter = express.Router();
 const authentication = require("../middlewares/authentication");
-const { createNewPackage } = require("../controllers/package");
+const { createNewPackage,createNewServicePackage } = require("../controllers/package");
 
 //this function for create a new package 
 //endpoint=>/package/create
 //body=>{ package_Name,price,Description,image,event }
 packageRouter.post("/create", createNewPackage);//admin
 
-
+//this function for create a new package 
+//endpoint=>/package/create/servicePackage
+//body=> { service_id,package_id }
+packageRouter.post("/create/servicePackage", createNewServicePackage);//admin
 module.exports = packageRouter;
