ce26bc0df93df4d9542ad79eee52f994441f192b
AmalHawwari <Amal.Alhawwari@gmail.com>
2024-02-15 18:28:55 +0300

    deleteServices

--- backend/controllers/service.js
+++ backend/controllers/service.js
@@ -83,8 +83,91 @@ const getAllServices = (req, res) => {
       });
     });
 };
+
+// this function to Get all service by provider id
+// EndPoint : GET /service/provider/:id
+const getServiceByProviderId = (req, res) => {
+  const id = req.params.id;
+  pool
+    .query(`SELECT * FROM services WHERE provider =$1 AND is_deleted=0`, [id])
+    .then((result) => {
+      if (result.rows.length === 0) {
+        res.status(404).json({
+          success: false,
+          message: `No Services Found for this provider ${id}!`,
+        });
+      }
+      res.status(200).json({
+        success: true,
+        message: `All the services for this provider ${id}`,
+        services: result.rows,
+      });
+    })
+    .catch((err) => {
+      res.status(500).json({
+        success: false,
+        message: "Server error",
+        err: err.message,
+      });
+    });
+};
+
+// this function to delete a service By id
+// EndPoint : GET /service/:id
+const deleteServiceById = (req, res) => {
+  const id = req.params.id;
+  const userId = req.token.userId;
+  pool
+    .query(
+      "UPDATE services SET is_deleted=1 WHERE service_id=$1 AND status='pending' AND provider =$2 RETURNING * ",
+      [id, userId]
+    )
+    .then((result) => {
+      if (result.rows.length !== 0) {
+        res.status(200).json({
+          success: true,
+          message: `Service with id: ${id} deleted successfully`,
+        });
+      } else {
+        pool
+          .query(
+            "SELECT * FROM services WHERE service_id=$1 AND provider =$2",
+            [id, userId]
+          )
+          .then((result) => {
+            if (result.rows.length !== 0) {
+              res.status(400).json({
+                success: false,
+                message: `Cannot delete service with id: ${id} because its status is not 'pending'`,
+              });
+            } else {
+              res.status(404).json({
+                success: false,
+                message: `Service with id: ${id} not found`,
+              });
+            }
+          })
+          .catch((err) => {
+            res.status(500).json({
+              success: false,
+              message: "Server error",
+              err: err,
+            });
+          });
+      }
+    })
+    .catch((err) => {
+      res.status(500).json({
+        success: false,
+        message: "Server error",
+        err: err,
+      });
+    });
+};
 module.exports = {
   createService,
   getAllServices,
   getServiceByName,
+  getServiceByProviderId,
+  deleteServiceById,
 };
--- backend/routers/service.js
+++ backend/routers/service.js
@@ -4,6 +4,8 @@ const {
   createService,
   getAllServices,
   getServiceByName,
+  getServiceByProviderId,
+  deleteServiceById,
 } = require("../controllers/service");
 
 const authentication = require("../middlewares/authentication");
@@ -14,5 +16,7 @@ serviceRouter.post("/", authentication, createService);
 serviceRouter.get("/", getAllServices);
 
 serviceRouter.get("/byName", getServiceByName);
+serviceRouter.get("/provider/:id", authentication,getServiceByProviderId)
+serviceRouter.delete("/:id", authentication, deleteServiceById)
 
 module.exports = serviceRouter;
