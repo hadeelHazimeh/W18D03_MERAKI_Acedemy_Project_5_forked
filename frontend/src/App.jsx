e6bdbfdce124ec5e267757388b256381b1f68b4f
unknown <hadeel.f.hazimeh@gmail.com>
2024-02-17 20:47:35 +0300

    frontEnd

---
+++ frontend/src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+// import './App.css'
+import 'mdb-react-ui-kit/dist/css/mdb.min.css';
+
+import Login from './pages/login'
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+  <>
+  <Login/>
+  
+  </>
+      
+  )
+}
+
+export default App
