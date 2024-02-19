61079593365130c20b4f81f8ce0a9b805b19e954
unknown <hadeel.f.hazimeh@gmail.com>
2024-02-19 21:26:11 +0300

    render services in order form

--- frontend/src/pages/client/index.jsx
+++ frontend/src/pages/client/index.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect } from "react";
-import { MDBInput, MDBBtn } from 'mdb-react-ui-kit';
+import { MDBInput, MDBBtn,MDBRow,MDBCard, MDBCardBody, MDBCardTitle, MDBCardText } from 'mdb-react-ui-kit';
 import { useSelector } from "react-redux";
 import axios from "axios";
 import { useState } from "react";
@@ -9,6 +9,7 @@ import serviceProvider from "../../services/redux/reducer/serviceProvider";
 const Client = () => {
   const { isLoggedIn } = useSelector((state) => state.auth);
   const [status, setStatus] = useState(false);
+  const[orderId,setOderId]=useState("")
   const token = useSelector((state) => state.auth.token);
   const [services, setServices] = useState([]);
   const [orderData, setOrderData] = useState({
@@ -31,7 +32,7 @@ const Client = () => {
       .then((result) => {
         console.log("services", result.data);
         setServices(result.data.services);
-        
+        setOderId(result.data.order_id)
         
       })
       .catch((err) => {
@@ -67,6 +68,7 @@ const Client = () => {
         plan your event By <a>services</a> or choose from our <a>packges</a>
       </h1>
       <form onSubmit={handleSubmit}>
+        <MDBRow className='mb-4'>
       <MDBInput
         label="Event Date"
         type="date"
@@ -74,7 +76,8 @@ const Client = () => {
         name="eventDate"
         value={orderData.eventDate}
         onChange={handleInputChange}
-      />
+      /></MDBRow>
+       <MDBRow className='mb-4'>
       <MDBInput
         label="Place"
         type="text"
@@ -82,8 +85,26 @@ const Client = () => {
         name="place"
         value={orderData.place}
         onChange={handleInputChange}
-      />
-  
+      /> </MDBRow>
+  <div style={{ display: 'flex', flexWrap: 'wrap', gap: '20px' }}>
+      {services.map((service) => (
+        <MDBCard key={service.id} style={{ maxWidth: '20rem' }}>
+          <MDBCardBody>
+            <MDBCardTitle>{service.name}</MDBCardTitle>
+            <MDBCardText>
+              Price: ${service.price}
+              <br />
+              {service.description}
+            </MDBCardText>
+            <MDBBtn color="primary">Select</MDBBtn>
+          </MDBCardBody>
+        </MDBCard>
+      ))}
+    </div>
+    <MDBRow className='mb-4'>
+      <MDBBtn className='mb-4' onClick={handleSubmit} block>
+        submit your plan
+      </MDBBtn></MDBRow>
       {/* <label htmlFor="service_ids">Select Services:</label>
       <select
         id="service_ids"
@@ -98,7 +119,7 @@ const Client = () => {
         ))}
       </select> */}
       {/* <p>Total Price: ${orderPrice}</p> */}
-      <MDBBtn type="submit">Create Order</MDBBtn>
+     
     </form>
       
     </div>
