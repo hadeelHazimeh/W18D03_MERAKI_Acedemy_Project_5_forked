d6fc1ef09f218ba2b5b2987767035ea2464a3396
Bushraamarat <amratbushra@gmail.com>
2024-02-19 18:20:50 +0300

    Title

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,7 +5,10 @@ import { router } from './Routers';
 // import './App.css'
 import 'mdb-react-ui-kit/dist/css/mdb.min.css';
 function App() {
-  return <RouterProvider router={router}/>
+  return <>
+  <RouterProvider router={router}/>
+  
+  </>
 }
 
 export default App
