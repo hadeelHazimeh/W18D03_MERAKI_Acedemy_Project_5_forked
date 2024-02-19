06a1411bc79e0716e82f1103e2a998debdb2395f
Bushraamarat <amratbushra@gmail.com>
2024-02-19 21:06:54 +0300

    homeChanges1

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,13 @@
  import { useState } from 'react'
 import { RouterProvider } from 'react-router-dom';
 import { router } from './Routers';
-
+import Navbar from './components/Navbar';
 // import './App.css'
 import 'mdb-react-ui-kit/dist/css/mdb.min.css';
 function App() {
   return <>
   <RouterProvider router={router}/>
-  
+  <Navbar/>
   </>
 }
 
