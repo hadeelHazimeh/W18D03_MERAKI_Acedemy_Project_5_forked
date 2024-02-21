90de309407c8e2db3ef9adabac91f252235f42e9
AyaAlomarii <ayah.aliomari@gmail.com>
2024-02-21 23:14:34 +0300

    edit get package fun

--- frontend/src/pages/Package/index.jsx
+++ frontend/src/pages/Package/index.jsx
@@ -1,6 +1,30 @@
-import React from 'react'
-
+import React, { useEffect } from "react";
+import { MDBInput, MDBBtn,MDBRow,MDBCard, MDBCardBody, MDBCardTitle, MDBCardText,MDBCardImage, MDBDropdown, MDBDropdownMenu, MDBDropdownToggle, MDBDropdownItem  } from 'mdb-react-ui-kit';
+import { useSelector } from "react-redux";
+import axios from "axios";
+import { useState } from "react";
+import { setPackages } from "../../services/redux/reducer/packages";
 const Packages= () => {
+  const { isLoggedIn,token } = useSelector((state) => state.auth);
+
+  //-----------------------------
+  useEffect(() => {
+      axios
+        .get(`http://localhost:5000/package/servicePackage`, {
+          
+        })
+        .then((result) => {
+          console.log("services", result.data.result);
+          
+          
+        })
+        .catch((err) => {
+          
+          console.log(err);
+  
+        });
+    }, []);
+
   return (
     <div>index</div>
   )
