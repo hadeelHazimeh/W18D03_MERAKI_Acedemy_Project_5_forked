45bcb685a57745007645df476a8912866f0b84e4
Bushraamarat <amratbushra@gmail.com>
2024-02-21 13:01:01 +0300

    editNav6

--- frontend/src/components/Navbar/index.jsx
+++ frontend/src/components/Navbar/index.jsx
@@ -17,10 +17,73 @@ import {
 const Navbar = () => {
   const [openNavSecond, setOpenNavSecond] = useState(false);
   return (
-    <>
-<div>
+    
+    <div>
 
-      <MDBNavbar light bgColor='light'>
+    <MDBNavbar expand='lg' light bgColor='light'>
+      <MDBContainer fluid>
+        <div style={{display:'flex',justifyContent: 'space-between'}}>
+      <MDBNavbarBrand href='/'>
+            <img
+              src='https://res.cloudinary.com/dycpjbdto/image/upload/v1708504176/2-removebg-preview-less_uolwak.png'
+              height='50'
+              width='90%'
+              alt=''
+              loading='lazy'
+            />
+          </MDBNavbarBrand>
+          </div>
+        <MDBNavbarToggler
+          aria-expanded='false'
+          aria-label='Toggle navigation'
+          onClick={() => setOpenNavSecond(!openNavSecond)}
+        >
+          <MDBIcon icon='bars' fas />
+        </MDBNavbarToggler>
+        <div style={{display:"flex", justifyContent:'space-around' }}>
+        <MDBCollapse navbar open={openNavSecond}>
+          <MDBNavbarNav>
+            <MDBNavbarLink active aria-current='page' href='/'>
+              Home
+            </MDBNavbarLink>
+            <MDBNavbarToggler
+          aria-expanded='false'
+          aria-label='Toggle navigation'
+          onClick={() => setOpenNavSecond(!openNavSecond)}
+        >
+          <MDBIcon icon='bars' fas />
+        </MDBNavbarToggler>
+            <MDBNavbarLink href='/client'>Plan Your Event</MDBNavbarLink>
+            <MDBNavbarLink href='/portfolio'>PortFolio</MDBNavbarLink>
+            <MDBNavbarLink href='/letstalk'>Lets Talk!</MDBNavbarLink>
+            <MDBNavbarLink href='/AboutUs'>About Us</MDBNavbarLink>
+            <MDBNavbarLink href='/login' >
+                Are You Vendor?
+              </MDBNavbarLink>
+              <MDBDropdown>
+                <MDBDropdownToggle tag='a' className='nav-link' role='button'>
+Login As:                </MDBDropdownToggle>
+                <MDBDropdownMenu>
+                  <MDBDropdownItem  href='/login' link>Event Planner</MDBDropdownItem>
+                  <MDBDropdownItem href='/login' link>Client</MDBDropdownItem>
+                </MDBDropdownMenu>
+              </MDBDropdown>
+          </MDBNavbarNav>
+        </MDBCollapse>
+        </div>
+      </MDBContainer>
+    </MDBNavbar>
+
+
+
+
+    </div>
+    )
+    }
+    export default Navbar;
+{/* <div> */}
+
+      {/* <MDBNavbar light bgColor='light'>
         <MDBContainer>
           <MDBNavbarBrand href='/'>
             <img
@@ -32,6 +95,39 @@ const Navbar = () => {
             />
           </MDBNavbarBrand>
         
+          <MDBContainer fluid>
+      <MDBNavbarLink active aria-current='page' href='/'>
+              Home
+            </MDBNavbarLink>
+        <MDBNavbarToggler
+          aria-expanded='false'
+          aria-label='Toggle navigation'
+          onClick={() => setOpenNavSecond(!openNavSecond)}
+        >
+          <MDBIcon icon='bars' fas />
+        </MDBNavbarToggler>
+        <MDBCollapse navbar open={openNavSecond}>
+          <MDBNavbarNav>
+            <MDBNavbarLink active aria-current='page' href='/Client'>
+              Plan Your Event
+            </MDBNavbarLink>
+           
+            <MDBNavbarLink href='/portfolio'>PortFolio</MDBNavbarLink>
+            <MDBNavbarLink href='/letstalk'>Lets Talk!</MDBNavbarLink>
+            <MDBNavbarLink href='/AboutUs'>About us</MDBNavbarLink>
+
+          </MDBNavbarNav>
+        </MDBCollapse> 
+              <MDBDropdown>
+                <MDBDropdownToggle tag='a' className='nav-link' role='button'>
+Login As:                </MDBDropdownToggle>
+                <MDBDropdownMenu>
+                  <MDBDropdownItem  href='/login' link>Admin</MDBDropdownItem>
+                  <MDBDropdownItem href='/login' link>Client</MDBDropdownItem>
+                </MDBDropdownMenu>
+              </MDBDropdown>
+
+      </MDBContainer>
           <MDBNavbarLink href='/login' >
                 Are You Vendor?
               </MDBNavbarLink>
@@ -40,9 +136,9 @@ const Navbar = () => {
         
       </MDBNavbar>
     
-</div>
+</div> */}
 
-<div>
+{/* <div>
 <MDBNavbar expand='lg' light bgColor='light'>
       <MDBContainer fluid>
       <MDBNavbarLink active aria-current='page' href='/'>
@@ -57,8 +153,8 @@ const Navbar = () => {
         </MDBNavbarToggler>
         <MDBCollapse navbar open={openNavSecond}>
           <MDBNavbarNav>
-            <MDBNavbarLink active aria-current='page' href='/serviceClient'>
-              Services
+            <MDBNavbarLink active aria-current='page' href='/Client'>
+              Plan Your Event
             </MDBNavbarLink>
            
             <MDBNavbarLink href='/portfolio'>PortFolio</MDBNavbarLink>
@@ -71,22 +167,19 @@ const Navbar = () => {
                 <MDBDropdownToggle tag='a' className='nav-link' role='button'>
 Login As:                </MDBDropdownToggle>
                 <MDBDropdownMenu>
-                  <MDBDropdownItem link>Action</MDBDropdownItem>
-                  <MDBDropdownItem link>Another action</MDBDropdownItem>
-                  <MDBDropdownItem link>Something else here</MDBDropdownItem>
+                  <MDBDropdownItem  href='/login' link>Admin</MDBDropdownItem>
+                  <MDBDropdownItem href='/login' link>Client</MDBDropdownItem>
                 </MDBDropdownMenu>
               </MDBDropdown>
 
       </MDBContainer>
     </MDBNavbar>
     
-</div>
-</>
-     )
+</div> */}
 
-  }
+    
 
 
      
 
-export default Navbar;
+
