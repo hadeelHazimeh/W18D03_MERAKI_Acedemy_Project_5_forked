44e130cc62d7b6c188cda7063c007d5b0aae572f
Bushraamarat <amratbushra@gmail.com>
2024-02-21 13:37:24 +0300

    editnav7

--- frontend/src/pages/Home/index.jsx
+++ frontend/src/pages/Home/index.jsx
@@ -1,12 +1,13 @@
 const Home = () => {
   return (
-    <div>
-      Home
+    <div style={{objectFit:"contain"}}>
+      <video className="w-100" autoPlay loop muted>
+      <source
+        src="https://mdbootstrap.com/img/video/animation-intro.mp4"
+        type="video/mp4"
+      />
+    </video>
    
-    
-       
-   
-    
     </div>
     
     
