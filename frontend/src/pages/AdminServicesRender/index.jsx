e3f1670df25dad981b7f7de1918e8d8b48487ca8
AyaAlomarii <ayah.aliomari@gmail.com>
2024-02-21 14:27:27 +0300

    rendering services in admin dashbored

--- frontend/src/pages/AdminServicesRender/index.jsx
+++ frontend/src/pages/AdminServicesRender/index.jsx
@@ -1,8 +1,72 @@
-import React from 'react'
+import React, { useEffect } from "react";
+import { MDBInput, MDBBtn,MDBRow,MDBCard, MDBCardBody, MDBCardTitle, MDBCardText,MDBCardImage, MDBDropdown, MDBDropdownMenu, MDBDropdownToggle, MDBDropdownItem  } from 'mdb-react-ui-kit';
+import { useSelector } from "react-redux";
+import axios from "axios";
+import { useState } from "react";
+
 
 function AdminServicesRender() {
+    const { isLoggedIn,token } = useSelector((state) => state.auth);
+   
+    const [services, setServices] = useState([]);
+   
+    //-----------------------------
+    useEffect(() => {
+        axios
+          .get(`http://localhost:5000/service`, {
+            
+          })
+          .then((result) => {
+            console.log("services", result.data.services);
+            setServices(result.data.services);
+            
+            
+          })
+          .catch((err) => {
+            
+            console.log(err);
+    
+          });
+      }, []);
+    //...............................
   return (
-    <div>ad</div>
+    <> <div>
+    
+<div style={{ display: 'flex', flexWrap: 'wrap', gap: '20px' }}>
+    {services.map((service) => (
+      <MDBCard key={service.id} style={{ maxWidth: '20rem' }}>
+        <MDBCardBody>
+        <MDBCardImage
+            src={service.image}
+            alt="..."
+            position="top"
+          />
+          <MDBCardTitle>{service.name}</MDBCardTitle>
+          <MDBCardText>
+            Price: ${service.price}
+            <br />
+            {service.description}
+          </MDBCardText>
+          <MDBDropdown group>
+        <MDBDropdownToggle color='primary'>Action</MDBDropdownToggle>
+        <MDBDropdownMenu>
+          <MDBDropdownItem link>Action</MDBDropdownItem>
+          <MDBDropdownItem link>Another action</MDBDropdownItem>
+          <MDBDropdownItem link>Something else here</MDBDropdownItem>
+        </MDBDropdownMenu>
+      </MDBDropdown>
+     
+        </MDBCardBody>
+      </MDBCard>
+    ))}
+  </div>
+  <MDBRow className='mb-4'>
+    
+   </MDBRow>
+
+  
+    
+  </div></>
   )
 }
 
