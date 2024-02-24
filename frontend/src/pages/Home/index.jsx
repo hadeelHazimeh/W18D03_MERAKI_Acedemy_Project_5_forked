7fc7a37b80058418107c187005930792fbce52b6
Bushraamarat <amratbushra@gmail.com>
2024-02-24 07:57:17 +0300

    main changes

--- frontend/src/pages/Home/index.jsx
+++ frontend/src/pages/Home/index.jsx
@@ -1,17 +1,32 @@
 const Home = () => {
   return (
-    <div style={{objectFit:"contain"}}>
-      <video className="w-100" autoPlay loop muted>
-      <source
-        src="https://mdbootstrap.com/img/video/animation-intro.mp4"
-        type="video/mp4"
-      />
-    </video>
-   
+    // <div style={{objectFit:"contain"}}>
+    //    <video classNameName="w-100" autoPlay loop muted>
+    //   <source
+    //     src="https://mdbootstrap.com/img/video/animation-intro.mp4"
+    //     type="video/mp4"
+    //   />
+    // </video>
+    // </div>
+    <div>
+      <div
+        id="carouselExampleSlidesOnly"
+        className="carousel slide"
+        data-ride="carousel"
+      >
+        <div className="carousel-inner">
+          <div className="carousel-item active">
+          <video className="d-block w-100" autoPlay loop muted>
+       <source
+         src="https://mdbootstrap.com/img/video/animation-intro.mp4"
+         type="video/mp4"
+  />
+     </video>
+          </div>
+        </div>
+      </div>
     </div>
-    
-    
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
