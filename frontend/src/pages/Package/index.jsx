203ab4ac9669288bd80e5f66372ec40c750f42cc
AyaAlomarii <ayah.aliomari@gmail.com>
2024-02-26 01:29:55 +0300

    edits in render package page

--- frontend/src/pages/Package/index.jsx
+++ frontend/src/pages/Package/index.jsx
@@ -1,13 +1,28 @@
 import React, { useEffect,useState } from "react";
-import { MDBInput, MDBBtn,MDBRow,MDBCard, MDBCardBody, MDBCardTitle, MDBCardText,MDBCardImage, MDBDropdown, MDBDropdownMenu, MDBDropdownToggle, MDBDropdownItem ,MDBBadge, MDBListGroup, MDBListGroupItem,MDBCardHeader } from 'mdb-react-ui-kit';
+import { MDBInput, MDBBtn,MDBRow,MDBCard, MDBCardBody, MDBCardTitle, MDBCardText,MDBCardImage, MDBDropdown, MDBDropdownMenu, MDBDropdownToggle, MDBDropdownItem ,MDBBadge, MDBListGroup, MDBListGroupItem,MDBCardHeader, MDBRipple ,
+  MDBModal,
+  MDBModalDialog,
+  MDBModalContent,
+  MDBModalHeader,
+  MDBModalTitle,
+  MDBModalBody,
+  MDBModalFooter} from 'mdb-react-ui-kit';
+import { Button, Modal, Nav ,Image} from "react-bootstrap";
 
+import"./style.css"
 import { useDispatch, useSelector } from "react-redux";
 import axios from "axios";
 import { setPackages,setPackagesName } from "../../services/redux/reducer/packages";
 const Packages= () => {
-
+  const [basicModal, setBasicModal] = useState(false);
+  const toggleOpen = () => setBasicModal(!basicModal);
   const { packages, packagesName} = useSelector((state) => state.packages);
-
+  const [modalShow, setModalShow] = useState(false);
+const [serviceInfo, setServiceInfo] = useState({service_name:"",
+price:0,
+description:"",
+image:""
+})
   const { isLoggedIn,token } = useSelector((state) => state.auth);
 const dispatch=useDispatch()
   //-----------------------------
@@ -51,10 +66,11 @@ const dispatch=useDispatch()
 
   return (
     <div>
-      <div style={{ display: 'flex', flexWrap: 'wrap', gap: '20px', margin:"20px"}}>
+      
+      <div style={{ display: 'flex', flexWrap: 'wrap' ,alignContent:"center", gap: '20px', margin:"20px"}}>
    
     {packagesName.map((ele,i) => (
-      <MDBCard key={i} style={{ maxWidth: '20rem' }}>
+      <MDBCard key={i} style={{ maxWidth: '25rem' }}>
        
         <MDBCardBody>
         <MDBCardImage
@@ -72,15 +88,24 @@ package_name}</MDBCardTitle>
             Price: ${ele.price}
           </MDBCardText>
           <MDBCard>
-          <MDBCardHeader>services</MDBCardHeader>
+           
+          <MDBCardHeader style={{fontSize:"20px"}}>services</MDBCardHeader>
             {packages.map((pac,i)=>{
               if(ele.package_name===pac.package_name){
                 return(<div key={i}>
                    
-      <MDBListGroup flush>
-        <MDBListGroupItem>{pac.service_name}</MDBListGroupItem>
-     
+
+      <MDBListGroup style={{ minWidth: '22rem' }} light>
+      <MDBRipple>
+        <MDBListGroupItem onClick={()=>{setModalShow(true)
+           setServiceInfo(pac)}} style={{cursor: "pointer"}} href='#' action noBorders  aria-current='false' className='px-3'>
+        {pac.service_name}
+        </MDBListGroupItem>
+       
+      </MDBRipple>
+
       </MDBListGroup>
+
    </div>)
               }
             })}
@@ -89,7 +114,22 @@ package_name}</MDBCardTitle>
       </MDBCard>
     ))}
     </div>
-      
+   
+      <Modal show={modalShow} onHide={() => setModalShow(false)}>
+        <Modal.Header closeButton>
+          <Modal.Title>{serviceInfo.service_name}</Modal.Title>
+        </Modal.Header>
+        <Modal.Body className="modalshowing">
+        <Image src={serviceInfo.image} fluid />
+        <h5><span style={{fontWeight:"bold"}}>Price: </span> {serviceInfo.price}</h5>
+        <p>{serviceInfo.description}</p>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="dark" onClick={() => setModalShow(false)}>
+            Close
+          </Button>
+        </Modal.Footer>
+      </Modal>
     </div>
     
   )
