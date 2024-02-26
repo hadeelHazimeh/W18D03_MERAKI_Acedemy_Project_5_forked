3ae5d5a8c462e4a254517833a14042fdd3a17dbc
AyaAlomarii <ayah.aliomari@gmail.com>
2024-02-26 06:11:14 +0300

    get orders and render them in table

--- frontend/src/pages/adminOrder/index.jsx
+++ frontend/src/pages/adminOrder/index.jsx
@@ -8,12 +8,13 @@ import axios from 'axios';
 import { MDBBadge, MDBBtn, MDBTable, MDBTableHead, MDBTableBody } from 'mdb-react-ui-kit';
 
 export default function OrdersTable() {
+    const [orders, setOrders] = useState([])
     const getAllOrders=()=>{
         //http://localhost:5000/orders/allOrders axios
           axios.get(`http://localhost:5000/orders/allOrders`)
           .then((result) => {
            console.log(result.data.result);
-          
+          setOrders(result.data.result)
           })
           .catch((err) => {
             console.log(err);
@@ -24,111 +25,64 @@ getAllOrders()
 }, [])
 
   return (
+    <div  style={{margin:"10px", marginRight:"60px"}}>
     <MDBTable align='middle'>
       <MDBTableHead>
         <tr>
-          <th scope='col'>Name</th>
-          <th scope='col'>Title</th>
-          <th scope='col'>Status</th>
-          <th scope='col'>Position</th>
-          <th scope='col'>Actions</th>
+          <th scope='col'>User</th>
+          <th scope='col'>Event Name</th>
+          <th scope='col'>Event Date</th>
+          <th scope='col'>Place</th>
+          <th scope='col'>Price</th>
+          <th scope='col'>Service Name</th>
+
         </tr>
       </MDBTableHead>
       <MDBTableBody>
-        <tr>
-          <td>
-            <div className='d-flex align-items-center'>
-              <img
-                src='https://mdbootstrap.com/img/new/avatars/8.jpg'
-                alt=''
-                style={{ width: '45px', height: '45px' }}
-                className='rounded-circle'
-              />
-              <div className='ms-3'>
-                <p className='fw-bold mb-1'>John Doe</p>
-                <p className='text-muted mb-0'>john.doe@gmail.com</p>
-              </div>
-            </div>
-          </td>
-          <td>
-            <p className='fw-normal mb-1'>Software engineer</p>
-            <p className='text-muted mb-0'>IT department</p>
-          </td>
-          <td>
-            <MDBBadge color='success' pill>
-              Active
-            </MDBBadge>
-          </td>
-          <td>Senior</td>
-          <td>
-            <MDBBtn color='link' rounded size='sm'>
-              Edit
-            </MDBBtn>
-          </td>
-        </tr>
-        <tr>
-          <td>
-            <div className='d-flex align-items-center'>
-              <img
-                src='https://mdbootstrap.com/img/new/avatars/6.jpg'
-                alt=''
-                style={{ width: '45px', height: '45px' }}
-                className='rounded-circle'
-              />
-              <div className='ms-3'>
-                <p className='fw-bold mb-1'>Alex Ray</p>
-                <p className='text-muted mb-0'>alex.ray@gmail.com</p>
-              </div>
-            </div>
-          </td>
-          <td>
-            <p className='fw-normal mb-1'>Consultant</p>
-            <p className='text-muted mb-0'>Finance</p>
-          </td>
-          <td>
-            <MDBBadge color='primary' pill>
-              Onboarding
-            </MDBBadge>
-          </td>
-          <td>Junior</td>
-          <td>
-            <MDBBtn color='link' rounded size='sm'>
-              Edit
-            </MDBBtn>
-          </td>
-        </tr>
-        <tr>
-          <td>
-            <div className='d-flex align-items-center'>
-              <img
-                src='https://mdbootstrap.com/img/new/avatars/7.jpg'
-                alt=''
-                style={{ width: '45px', height: '45px' }}
-                className='rounded-circle'
-              />
-              <div className='ms-3'>
-                <p className='fw-bold mb-1'>Kate Hunington</p>
-                <p className='text-muted mb-0'>kate.hunington@gmail.com</p>
-              </div>
-            </div>
-          </td>
-          <td>
-            <p className='fw-normal mb-1'>Designer</p>
-            <p className='text-muted mb-0'>UI/UX</p>
-          </td>
-          <td>
-            <MDBBadge color='warning' pill>
-              Awaiting
-            </MDBBadge>
-          </td>
-          <td>Senior</td>
-          <td>
-            <MDBBtn color='link' rounded size='sm'>
-              Edit
-            </MDBBtn>
-          </td>
-        </tr>
+        {orders.map((order,i)=>{
+  return (          
+ <tr>
+ <td>
+   <div className='d-flex align-items-center'>
+     <img
+       src={order.image}
+       alt=''
+       style={{ width: '45px', height: '45px' }}
+       className='rounded-circle'
+     />
+     <div className='ms-3'>
+       <p className='fw-bold mb-1'>{order.username}</p>
+       <p className='text-muted mb-0'>{order.email}</p>
+     </div>
+   </div>
+ </td>
+ <td>
+   <p className='fw-normal mb-1'>{order.event_name}</p>
+   
+ </td>
+ <td>
+ <p className='fw-normal mb-1'>{order.eventdate}</p>
+ </td>
+ <td>
+    
+ <p className='fw-normal mb-1'>{order.place}</p>
+ </td>
+ <td>
+ <p className='fw-normal mb-1'>{order.price}</p>
+
+ </td>
+ <td>
+ <p className='fw-normal mb-1'>{order.service_name}</p>
+
+ </td>
+
+
+</tr>)
+        })}
+       
+   
       </MDBTableBody>
     </MDBTable>
+    </div>
   );
 }
