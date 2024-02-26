90486dd0739f0679f6df5b5996c401316a9b118a
AyaAlomarii <ayah.aliomari@gmail.com>
2024-02-26 06:37:51 +0300

    side bar style edit

--- frontend/src/components/AdminSideBar/index.jsx
+++ frontend/src/components/AdminSideBar/index.jsx
@@ -6,30 +6,37 @@ import {
   CDBSidebarMenuItem,
   CDBSidebarFooter,
 } from 'cdbreact';
+import { useSelector } from 'react-redux';
+import {MDBIcon} from "mdb-react-ui-kit"
+
 import { NavLink } from 'react-router-dom';
 const AdminSideBar = () => {
+  const name = useSelector(
+    (state) => state.auth.userName
+  );
     return (
-        <CDBSidebar /*  style={{backgroundColor:"white",color:"#ACDAD1"}} */ >
-          <CDBSidebarHeader prefix={<i className="fa fa-bars" />}>Contrast</CDBSidebarHeader>
+        <CDBSidebar style={{ backgroundColor: "#302B2B" }}>
+          
+          <CDBSidebarHeader prefix={<i className="fa fa-bars" />}>Hello {name}</CDBSidebarHeader>
           <CDBSidebarContent>
             <CDBSidebarMenu>
-              <NavLink to="pending/Services"><CDBSidebarMenuItem icon="th-large">Pending Services</CDBSidebarMenuItem></NavLink>
-             <NavLink to={"packages"}> <CDBSidebarMenuItem icon="sticky-note">Components</CDBSidebarMenuItem></NavLink>
-             <NavLink to={"Services"}> <CDBSidebarMenuItem icon="th-large" iconType="solid">
-                Services
+              <NavLink to="pending/Services"><CDBSidebarMenuItem icon="lock-open">Pending Services</CDBSidebarMenuItem></NavLink>
+             <NavLink to={"packages"}> <CDBSidebarMenuItem icon="th-large">Packages</CDBSidebarMenuItem></NavLink>
+             <NavLink to={"Services"}> <CDBSidebarMenuItem icon="plus-square" iconType="solid">
+                Create Package
               </CDBSidebarMenuItem></NavLink>
-              <NavLink to={"orders"}> <CDBSidebarMenuItem icon="th-large" iconType="solid">
+              <NavLink to={"orders"}> <CDBSidebarMenuItem icon="shopping-bag" iconType="solid">
                 All Orders
               </CDBSidebarMenuItem></NavLink>
             </CDBSidebarMenu>
           </CDBSidebarContent>
-  
+         
           <CDBSidebarFooter style={{ textAlign: 'center' }}>
             <div
               className="sidebar-btn-wrapper"
               style={{padding: '20px 5px'}}
             >
-              Sidebar Footer
+              
             </div>
           </CDBSidebarFooter>
         </CDBSidebar>
