1d6a01dc7b5acc15df0a0b66713478d8b523462c
AyaAlomarii <ayah.aliomari@gmail.com>
2024-02-27 01:21:09 +0300

    adding style to not authorized error

--- frontend/src/pages/AdminDashbored/index.jsx
+++ frontend/src/pages/AdminDashbored/index.jsx
@@ -1,8 +1,17 @@
 import React, { useState } from 'react'
+import {
+  MDBCard,
+  MDBCardBody,
+  MDBCardTitle,
+  MDBCardText,
+  MDBCardImage
+} from 'mdb-react-ui-kit';
+
 import AdminSideBar from '../../components/AdminSideBar'
 import { useSelector } from 'react-redux'
 import { Outlet } from 'react-router-dom'
 import { useNavigate } from 'react-router-dom'
+import { Container } from 'react-bootstrap';
 const AdminDashboard = () => {
   const { isLoggedIn,token,role } = useSelector((state) => state.auth);
 const [roleLocal, setRoleLocal] = useState(localStorage.getItem("role"))
@@ -14,7 +23,14 @@ const [roleLocal, setRoleLocal] = useState(localStorage.getItem("role"))
     <div> <Outlet/></div>
        
     
-    </div></>:<><p>your not an admin </p></>
+    </div></>:<><Container style={{display:"flex",justifyContent:"center"}} className=" text-center  service-container" ><MDBCard  style={{maxWidth:"70rem" ,justifyContent:"center",marginTop:"30px" ,backgroundColor:"#302B2B"}} className='mb-32'>
+        <MDBCardImage position='top' src='https://cdn.dribbble.com/users/959295/screenshots/2721842/404-inspiration.gif' alt='...' />
+        <MDBCardBody>
+          <MDBCardTitle style={{textAlign:"center",fontWeight:"bold",fontFamily:"Raleway",color:"whitesmoke"}}>Your Not Authorized to access this page </MDBCardTitle>
+      
+        </MDBCardBody>
+      </MDBCard></Container> 
+</>
     
    }</>: <> {navigate("/login")} </> }
    
