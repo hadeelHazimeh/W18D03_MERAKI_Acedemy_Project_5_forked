d52ab4a421a61123ee48d491f11ffc570770af43
Bushraamarat <amratbushra@gmail.com>
2024-02-26 03:42:34 +0300

    edit

--- frontend/src/pages/Home/index.jsx
+++ frontend/src/pages/Home/index.jsx
@@ -1,32 +1,38 @@
 import "./index.css";
+import Carousel from 'react-bootstrap/Carousel';
+
 const Home = () => {
   return (
     <div>
-      <div className="video-container">
-        <video className="video-background" autoPlay muted controls loop>
+      <div classNameName="video-container">
+        <video classNameName="video-background" autoPlay muted controls loop>
           <source
             src="https://res.cloudinary.com/dycpjbdto/video/upload/v1708783111/km_farah_720p_50f_20240224_142639_lkpe5z.3gp"
             type="video/mp4"
           />
         </video>
       </div>
-      <div className="card bg-dark ">
-  <img src="https://res.cloudinary.com/dycpjbdto/image/upload/v1708823786/pexels-alena-koval-961402_eymjio.jpg" className="card-img" alt="..."/>
-  <div className="card-img-overlay">
+      <div classNameName="card bg-dark ">
+  <img src="https://res.cloudinary.com/dycpjbdto/image/upload/v1708823786/pexels-alena-koval-961402_eymjio.jpg" classNameName="card-img" alt="..."/>
+  <div classNameName="card-img-overlay">
     <br/><br/>
-    <h5 className="card-title" ><strong>Your Vision +
+    <h5 classNameName="card-title" ><strong>Your Vision +
 Our Expertise = The Perfect Partnership</strong></h5>
 <br/>
 
-    <p className="card-text">Our creativity, attention to detail, and passion for planning <br/>
+    <p classNameName="card-text">Our creativity, attention to detail, and passion for planning <br/>
     will ensure that your next corporate event is a huge success.<br/>
      We take the time to get to know your organization – your mission,  <br/>your goals, your vision,
      and we work with you to create an event that reflects your company brand.</p>
      <br/>
-     <a class="button arrow" href="/portfolio">OUR WORK</a>
+     <a className="button arrow" href="/portfolio">OUR WORK</a>
   </div>
 
 </div>
+
+
+
+
 
     </div>
   );
