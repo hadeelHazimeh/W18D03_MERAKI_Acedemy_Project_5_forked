97eb6da3a36a5bfc83e8b8cb799197a07de0d6af
AyaAlomarii <ayah.aliomari@gmail.com>
2024-02-27 00:23:09 +0300

    edit style for packages page

--- frontend/src/pages/Package/index.jsx
+++ frontend/src/pages/Package/index.jsx
@@ -7,7 +7,7 @@ import { MDBInput, MDBBtn,MDBRow,MDBCard, MDBCardBody, MDBCardTitle, MDBCardText
   MDBModalTitle,
   MDBModalBody,
   MDBModalFooter} from 'mdb-react-ui-kit';
-import { Button, Modal, Nav ,Image} from "react-bootstrap";
+import { Button, Modal, Nav ,Image, CardLink, CardText, CardTitle} from "react-bootstrap";
 
 import"./style.css"
 import { useDispatch, useSelector } from "react-redux";
@@ -66,11 +66,76 @@ const dispatch=useDispatch()
 
   return (
     <div>
+      {/* <div style={{ display: 'flex', flexWrap: 'wrap', justifyContent: 'space-between' ,margin:"30px"}}>
+          {Packages.map((service) => (
+            <MDBCard  style={{ width: 'calc(30.33% - 20px)', marginBottom: '30px',backgroundColor:"#f3f1ec",margin:"10px",  }}>
+              <MDBCardBody>
+
+                <MDBCardImage
+                  src={service.image}
+                  alt="..."
+                  position="top"
+                  className="cardImg"
+                />
+                <MDBCardTitle>
+                  <p
+                    style={{
+                      textAlign: "",
+                      fontFamily: "Raleway",
+                      borderBottom: "1px solid #302B2B",
+                    }}
+                  >
+                    {" "}
+                    {service.service_name}
+                  </p>
+                </MDBCardTitle>
+                <br />
+                <MDBCardText>
+                  <p
+                    style={{
+                      textAlign: "justify",
+                      borderBottom: "1px  #00A3AF ",
+                      fontFamily: "Raleway",
+                      fontSize:"20px"
+                    }}
+                  >
+                    <strong>Price:</strong>{" "}
+                    <span style={{ display: "inline" }}>
+                      JD {service.price}
+                    </span>
+                  </p>
+                  <p
+                    style={{
+                      textAlign: "justify",
+                     paddingBottom:"10px",
+                      fontFamily: "Merriweather",
+                    }}
+                  >
+                    <strong>Description:</strong>{" "}
+                    <span style={{ display: "inline" }}>{service.details}</span>
+                  </p>
+                </MDBCardText>
+                <MDBBtn style={{backgroundColor:"#302B2B"}} onClick={()=>{
+        handelStatus("confirmed",service.service_id)
+      }} color='white' outline>
+        Confirm
+      </MDBBtn>
       
-      <div style={{ display: 'flex', flexWrap: 'wrap' ,alignContent:"center", gap: '20px', margin:"20px"}}>
+      <MDBBtn style={{backgroundColor:"#302B2B"}} onClick={()=>{
+        handelStatus("Rejected",service.service_id)
+      }} color='white' outline>
+        Reject
+      </MDBBtn>
+              </MDBCardBody>
+            </MDBCard>
+          ))}
+        </div> */}
+         
+          
+      <div  style={{ display: 'flex', flexWrap: 'wrap', justifyContent: 'space-around' ,margin:"30px"}}>
    
     {packagesName.map((ele,i) => (
-      <MDBCard key={i} style={{ maxWidth: '25rem' }}>
+      <MDBCard key={i} style={{  width: 'calc(36.33% - 20px)', marginBottom: '30px',backgroundColor:"#f3f1ec",margin:"10px" }}>
        
         <MDBCardBody>
         <MDBCardImage
@@ -78,53 +143,144 @@ const dispatch=useDispatch()
             alt="..."
             position="top"
           />
-          <MDBCardTitle>{ele.
+          <MDBCardTitle style={{
+                      textAlign: "",
+                      fontFamily: "Raleway",
+                      borderBottom: "1px solid #302B2B",
+                    }}>{ele.
 package_name}</MDBCardTitle>
 <MDBCardText>
             
-            
-            {ele.description}
-            <br />
-            Price: ${ele.price}
-          </MDBCardText>
-          <MDBCard>
+<p
+                    style={{
+                      textAlign: "justify",
+                      borderBottom: "1px  #00A3AF ",
+                      fontFamily: "Raleway",
+                      fontSize:"20px"
+                    }}
+                  >
+                    <strong>Price:</strong>{" "}
+                    <span style={{ display: "inline" }}>
+                      {ele.price} JD 
+                    </span>
+                  </p>
+                  <p
+                    style={{
+                      textAlign: "justify",
+                     paddingBottom:"10px",
+                      fontFamily: "Merriweather",
+                    }}
+                  >
+                    <strong>Description:</strong>{" "}
+                    <span style={{ display: "inline" }}> {ele.description}</span>
+                  </p>
            
-          <MDBCardHeader style={{fontSize:"20px"}}>services</MDBCardHeader>
+           
+          
+          </MDBCardText>
+        
             {packages.map((pac,i)=>{
               if(ele.package_name===pac.package_name){
-                return(<div key={i}>
-                   
+                return(<div style={{
+                  fontFamily:"Raleway",
+                  fontWeight:"bold",
+                    position: "",
+                    bottom: "px", 
+                    left: "px",
+                    }} key={i}>
+                      
+                      
+                      <div>
+                     
+<p onClick={()=>{setModalShow(true)
+           setServiceInfo(pac)}} style={{cursor: "pointer",
+            fontFamily:"Raleway",
+            fontWeight:"normal",
+            backgroundColor:"",
+             border:"1px solid grey",
+           
+            height:"50px",
+            textAlign:"center",
+            margin:"3px",
+            paddingTop:"10px"
+            
+             
+              }}> {pac.service_name}</p>
+              
+              </div>
 
-      <MDBListGroup style={{ minWidth: '22rem' }} light>
+
+
+   {/*    <MDBListGroup style={{ minWidth: 'fit-content' }} light>
       <MDBRipple>
         <MDBListGroupItem onClick={()=>{setModalShow(true)
-           setServiceInfo(pac)}} style={{cursor: "pointer"}} href='#' action noBorders  aria-current='false' className='px-3'>
+           setServiceInfo(pac)}} style={{cursor: "pointer",
+            fontFamily:"Raleway",
+            fontWeight:"normal",
+            backgroundColor:"",
+             border:"1px solid grey",
+            
+             
+              }} href='#' action  aria-current='false' className='px-3'>
         {pac.service_name}
         </MDBListGroupItem>
        
       </MDBRipple>
 
-      </MDBListGroup>
+      </MDBListGroup> */}
 
    </div>)
               }
             })}
-      </MDBCard>
+   
         </MDBCardBody> 
       </MDBCard>
     ))}
     </div>
    
       <Modal show={modalShow} onHide={() => setModalShow(false)}>
-        <Modal.Header closeButton>
-          <Modal.Title>{serviceInfo.service_name}</Modal.Title>
+        <Modal.Header style={{  backgroundColor:"#302B2B"}} closeButton>
+          <Modal.Title style={{
+                      textAlign: "",
+                      fontFamily: "Raleway",
+                      borderBottom: "1px  ",
+                      color:"white"
+                    
+                    }}>{serviceInfo.service_name}</Modal.Title>
         </Modal.Header>
-        <Modal.Body className="modalshowing">
+        <Modal.Body  >
         <Image src={serviceInfo.image} fluid />
-        <h5><span style={{fontWeight:"bold"}}>Price: </span> {serviceInfo.price}</h5>
-        <p>{serviceInfo.description}</p>
+
+        <p
+                    style={{
+                      textAlign: "justify",
+                      borderBottom: "1px  #00A3AF ",
+                      fontFamily: "Raleway",
+                      fontSize:"20px",
+                      margin:"3px",
+                     
+                    }}
+                  >
+                    <strong>Price:</strong>{" "}
+                    <span style={{ display: "inline" }}>
+                      {serviceInfo.price} JD 
+                    </span>
+                  </p>
+                  <p
+                    style={{
+                      textAlign: "justify",
+                     paddingBottom:"10px",
+                      fontFamily: "Raleway",
+                      borderBottom:"none"
+                    }}
+                  >
+                    <strong>Description:</strong>{" "}
+                    <span style={{ display: "inline" }}> {serviceInfo.description}</span>
+                  </p>
+           
+       
         </Modal.Body>
-        <Modal.Footer>
+        <Modal.Footer >
           <Button variant="dark" onClick={() => setModalShow(false)}>
             Close
           </Button>
