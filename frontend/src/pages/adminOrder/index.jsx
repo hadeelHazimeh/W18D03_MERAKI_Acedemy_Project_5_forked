98877b25aaadad68b4bf657cafab93cd85e37372
AyaAlomarii <ayah.aliomari@gmail.com>
2024-02-26 21:07:08 +0300

    style edit

--- frontend/src/pages/adminOrder/index.jsx
+++ frontend/src/pages/adminOrder/index.jsx
@@ -1,10 +1,11 @@
 
-
-        
 import React, { useState, useEffect } from 'react';
-
+import { DataTable } from 'primereact/datatable';
+import { Column } from 'primereact/column';
+import { InputText } from 'primereact/inputtext';
+import { Tag } from 'primereact/tag';
 import axios from 'axios';
-
+import './style.css'; 
 import { MDBBadge, MDBBtn, MDBTable, MDBTableHead, MDBTableBody } from 'mdb-react-ui-kit';
 
 export default function OrdersTable() {
@@ -24,66 +25,81 @@ export default function OrdersTable() {
 useEffect(() => {
 getAllOrders()
 }, [])
+const formatCurrency = (value) => {
+  return value.toLocaleString("en-JO", {
+      style: "currency",
+      currency: "JOD",
+  });
+};
+
+const imageBodyTemplate = (rowData) => {
+  return (
+      <img
+      width={100}
+          src={rowData.image}
+          alt={rowData.service_name}
+          className="w-6rem shadow-2 border-round"
+      />
+  );
+};
+
+const priceBodyTemplate = (rowData) => {
+  return formatCurrency(rowData.price);
+};
+
+const eventDateBodyTemplate = (rowData) => {
+  return <span>{rowData.eventdate}</span>;
+};
+const eventNameBodyTemplate = (rowData) => {
+  return <span>{rowData.event_name}</span>;
+};
+const UserBodyTemplate = (rowData) => {
+  return <span>{rowData.username}<br/><span>{rowData.email}</span></span>;
+};
+const getSeverity = (status) => {
+  switch (status) {
+      case 'unqualified':
+          return 'danger';
+      case 'qualified':
+          return 'success';
+      case 'new':
+          return 'info';
+      case 'negotiation':
+          return 'warning';
+      case 'renewal':
+          return null;
+      default:
+          return null;
+  }
+};
+
+const statusBodyTemplate = (rowData) => {
+  return <Tag value={rowData.status} severity={getSeverity(rowData.status)} />;
+};
+
 
   return (
-    <div  style={{margin:"10px", marginRight:"60px"}}>
-    <MDBTable align='middle'>
-      <MDBTableHead>
-        <tr>
-          <th scope='col'>User</th>
-          <th scope='col'>Event Name</th>
-          <th scope='col'>Event Date</th>
-          <th scope='col'>Place</th>
-          <th scope='col'>Price</th>
-          <th scope='col'>Service Name</th>
+    <div  style={{margin:"10px", marginRight:"60px", height:"90vh"}}>
+      <div className="card col-10 m-5">
+            <DataTable
+                value={orders}
+                paginator
+                rows={8}
+            
+                emptyMessage="No orders found."
+                className=" custom-datatable" // Add your custom class for table styling
+            >
+                <Column className="text-center" header="Image" body={imageBodyTemplate} style={{width:"100px"}}></Column>
+                <Column className="text-center" header="user" body={UserBodyTemplate} style={{width:"200px"}}></Column>
+                <Column style={{width:"150px"}} field="service_name" header="Service Name"></Column>
+                <Column style={{width:"150px"}} field="price" header="Price" body={priceBodyTemplate}></Column>
+                <Column style={{width:"200px"}} field="eventName" header="Event Name" body={eventNameBodyTemplate}></Column>
+                <Column style={{width:"150px"}} field="eventdate" header="Event Date" body={eventDateBodyTemplate}></Column>
 
-        </tr>
-      </MDBTableHead>
-      <MDBTableBody>
-        {orders.map((order,i)=>{
-  return (          
- <tr>
- <td>
-   <div className='d-flex align-items-center'>
-     <img
-       src={order.image}
-       alt=''
-       style={{ width: '45px', height: '45px' }}
-       className='rounded-circle'
-     />
-     <div className='ms-3'>
-       <p className='fw-bold mb-1'>{order.username}</p>
-       <p className='text-muted mb-0'>{order.email}</p>
-     </div>
-   </div>
- </td>
- <td>
-   <p className='fw-normal mb-1'>{order.event_name}</p>
+                <Column style={{width:"100px"}} field="place" header="Place"></Column>
+            </DataTable>
+        </div>
    
- </td>
- <td>
- <p className='fw-normal mb-1'>{order.eventdate}</p>
- </td>
- <td>
-    
- <p className='fw-normal mb-1'>{order.place}</p>
- </td>
- <td>
- <p className='fw-normal mb-1'>{order.price}</p>
-
- </td>
- <td>
- <p className='fw-normal mb-1'>{order.service_name}</p>
-
- </td>
-
-
-</tr>)
-        })}
-       
-   
-      </MDBTableBody>
-    </MDBTable>
     </div>
   );
 }
